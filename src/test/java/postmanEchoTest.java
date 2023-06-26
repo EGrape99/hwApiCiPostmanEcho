df7017fb88c235fca425020a832baf2a4f2dbd6b
Elena Vinogradova <elena20031999@gmail.com>
2023-06-26 21:36:19 +0200

    Initial commit

---
+++ src/test/java/postmanEchoTest.java
@@ -0,0 +1,51 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class postmanEchoTest {
+
+    @Test
+    void shouldPostRequestsId() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Id")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Id"))
+        ;
+    }
+
+    @Test
+    void shouldPostRequestsName() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("name")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("name"))
+
+        ;
+    }
+
+    @Test
+    void shouldPostRequestsNumber() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("number")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("number"))
+
+        ;
+    }
+}
