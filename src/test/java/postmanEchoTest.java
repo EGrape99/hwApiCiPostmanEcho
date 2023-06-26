1c431a79ff6276b73f31396881c6154dcb6fd383
Elena Vinogradova <elena20031999@gmail.com>
2023-06-26 21:47:35 +0200

    incorrect term

--- src/test/java/postmanEchoTest.java
+++ src/test/java/postmanEchoTest.java
@@ -15,7 +15,7 @@ public class postmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Id"))
+                .body("data", equalTo("ID"))
         ;
     }
 
